88e29e954a05b9d85b9b2d8c27063098795e4c59
NikolaosKantartzopoulos <nkantartzopoulos@gmail.com>
2022-10-04 19:43:27 +0300

    Odin solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
-function App() {
-	return <div></div>;
+import React, { Component } from "react";
+import Overview from "./components/Overview";
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			task: { text: "" },
+			tasks: [],
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.onSubmitTask = this.onSubmitTask.bind(this);
+	}
+	handleChange = (e) => {
+		this.setState({
+			task: {
+				text: e.target.value,
+			},
+		});
+	};
+
+	onSubmitTask = (e) => {
+		e.preventDefault();
+		this.setState({
+			tasks: this.state.tasks.concat(this.state.task),
+			task: { text: "" },
+		});
+	};
+
+	render() {
+		const { task, tasks } = this.state;
+		return (
+			<div>
+				<form onSubmit={this.onSubmitTask}>
+					<input
+						onChange={this.handleChange}
+						value={task.text}
+						type="text"
+						id="taskInput"
+					/>
+					<button type="submit" task={"Submit"}>
+						Add task
+					</button>
+				</form>
+				<Overview tasks={tasks} />
+			</div>
+		);
+	}
 }
 
 export default App;
